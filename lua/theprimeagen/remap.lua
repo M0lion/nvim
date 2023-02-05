99686118eb52daa9f42fefe8d93ab8dbb15fae15
Bjørn Moholt <bjorn@moholt@hotmail.com>
2023-02-05 15:01:04 +0100

    Added terminal escape hotkey <C-C>

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -39,3 +39,5 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- make current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("t", "<C-C>", [[<C-\><C-n>]])
